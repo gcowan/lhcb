2b6d86345393c8e039ae39ceb9a6839b832f791b
Greig Cowan <greig.cowan@gmail.com>
2015-06-23 10:22:35 +0200

    Fixed problem with scripts for normalisation mode on data and changes to Bs2psi2Sphi resolution calibration

--- b2cc/analysis/Bs2psi2Sphi/resolution/src/plot_resolution_sim_2g_with_offset.C
+++ b2cc/analysis/Bs2psi2Sphi/resolution/src/plot_resolution_sim_2g_with_offset.C
@@ -7,6 +7,7 @@
 #include "RooCBShape.h"
 #include "RooVoigtian.h"
 #include "RooHistPdf.h"
+#include "RooHist.h"
 #include "RooProdPdf.h"
 #include "RooAddPdf.h"
 #include "RooAddModel.h"
@@ -65,16 +66,18 @@ void plot_resolution_sim_2g(){
 	TFile *file = TFile::Open("/afs/cern.ch/user/d/dferguso/public/output_ntupleB_presels_data_2012_prescale_sweightedbkg.root");
 	TTree *tree = (TTree*)file->Get("DecayTree");
 
-	RooRealVar *mdau1 = new RooRealVar("mdau1", "mdau1",3630.0,3740.0);
+	RooRealVar *mdau1 = new RooRealVar("mdau1", "dimuon mass",3630.0,3740.0, "MeV");
 	RooRealVar *hlt2_unbiased = new RooRealVar("hlt2_unbiased", "hlt2_unbiased",-10.0,10.0);
+	RooRealVar *muplus_PT = new RooRealVar("muplus_PT", "muplus_PT",0.0,100000.0);
+	RooRealVar *muminus_PT = new RooRealVar("muminus_PT", "muminus_PT",0.0,100000.0);
 	RooRealVar *time = new RooRealVar("time", "t",-1.5,8.0, "ps");
 	RooRealVar *sigmat = new RooRealVar("sigmat", "sigmat",0.005,0.12);
 	RooRealVar *sweight = new RooRealVar("sweight_bkg", "sweight_bkg", -10000.0, 100000.0);
-	RooArgList *fitArgs = new RooArgList(*time, *mdau1, *hlt2_unbiased, *sigmat, *sweight);
-	TCut cut1 = "(hlt2_unbiased==1)";
+	RooArgList *fitArgs = new RooArgList(*time, *mdau1, *hlt2_unbiased, *muplus_PT, *muminus_PT, *sigmat, *sweight);
+	TCut cut1 = "(hlt2_unbiased==1)&&(muplus_PT>1900&&muminus_PT>1900)";
 
 	time->setRange("res",-0.4, 0.4);
-	time->setRange("long",-1.0, 4.0);
+	time->setRange("long",-1.0, 8.0);
 
 	//RooDataSet * data1 = new RooDataSet("data1", "all input data", tree, *fitArgs, cut1, "sweight_bkg");
 	RooDataSet * data1 = new RooDataSet("data1", "all input data", tree, *fitArgs, cut1);
@@ -83,8 +86,8 @@ void plot_resolution_sim_2g(){
 	Double_t entries2 = data1->numEntries();
 	cout << entries2 << endl;
 
-	RooRealVar *mean_g1 = new RooRealVar("g1 #mu","mean of g1",0.00,-0.1, 0.1);
-	RooRealVar *mean_g2 = new RooRealVar("g2 #mu","mean of g2",0.00,-0.1, 0.1);
+	RooRealVar *mean_g1 = new RooRealVar("g1 #mu","mean of g1",0.00,-0.1, 0.2);
+	RooRealVar *mean_g2 = new RooRealVar("g2 #mu","mean of g2",0.00,-0.1, 0.5);
 	RooRealVar *sf1 = new RooRealVar("sf1", "sf1", 1.5, 0.1, 2.);
 	RooRealVar *sf2 = new RooRealVar("sf2", "sf2", 5.0, 2.0, 10.);
 	RooRealVar *cf1 = new RooRealVar("cf1", "cf1", 0.010, -0.1, 0.1);
@@ -92,7 +95,7 @@ void plot_resolution_sim_2g(){
 	RooRealVar *frac_g12 = new RooRealVar("frac_g12", "core fraction", 0.15, 0.01, 0.4);
 
 	RooRealVar *sigma_wide = new RooRealVar("#sigma wide","width of gwide",0.15,0.01,1.5);
-	RooRealVar *tau  = new RooRealVar("tau" , "tau" ,0.15, 0.1, 0.3);
+	RooRealVar *tau  = new RooRealVar("tau" , "tau" ,0.15, 0.1, 0.6);
 	RooRealVar *tau2 = new RooRealVar("tau2", "tau2",1.30, 0.5, 5.0);
 	RooRealVar *DG = new RooRealVar("DG", "DG",0.0);
 
@@ -104,7 +107,7 @@ void plot_resolution_sim_2g(){
 	RooGaussModel *gausswide = new RooGaussModel("gausswide","gauss",*time, *mean_g2,*sigma_wide);
 	RooRealVar *frac_gwide1 = new RooRealVar("frac_wide1", "core fraction wide1", 0.04, 0.0,0.1);
 	RooRealVar *frac_bdc1 = new RooRealVar("frac_bdc1", "core fraction2", 0.05,0.0,0.2);
-	RooRealVar *frac_tau2 = new RooRealVar("frac_tau2", "core fraction3", 0.05,0.0,0.1);
+	RooRealVar *frac_tau2 = new RooRealVar("frac_tau2", "core fraction3", 0.05,0.0,0.5);
 
 	RooBDecay *bdc1 = new RooBDecay("bdc1","bdc1",*time,*tau,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *res1, RooBDecay::SingleSided);
 	RooBDecay *bdc12 = new RooBDecay("bdc12","bdc12",*time,*tau2,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *res1, RooBDecay::SingleSided);
@@ -114,7 +117,13 @@ void plot_resolution_sim_2g(){
 	gROOT->ProcessLine(".x ~/lhcb/lhcbStyle.C");
 	RooFitResult *fitresult = total1->fitTo(*data1,Save(kTRUE), NumCPU(6),SumW2Error(kTRUE), ConditionalObservables(*sigmat)); 
 
-	RooPlot* frame1 = time->frame(Bins(60),Title("Prompt decay time1"), Range("res")) ;
+	RooPlot* frame0 = mdau1->frame(Bins(60),Title("m(mumu)"));
+	data1->plotOn(frame0);
+	TCanvas* c0 = new TCanvas("mdau1_plot","sim",600,800) ;
+	gPad->SetLeftMargin(0.15) ; frame0->Draw() ;
+	c0->SaveAs("plot_data_mdau1.pdf");
+    
+    RooPlot* frame1 = time->frame(Bins(60),Title("Prompt decay time1"), Range("res")) ;
 
 	// Plot all data tagged as physics sample
 	data1->plotOn(frame1) ;
@@ -122,7 +131,7 @@ void plot_resolution_sim_2g(){
 
 	RooHist* hpull1 = frame1->pullHist();
 	RooPlot* timepull1 = time->frame(Title("Pull Distribution"), Bins(60), Range("res")) ;
-	//timepull1->addPlotable(hpull1,"P") ;
+	timepull1->addPlotable(hpull1,"P") ;
 	TAxis * xachse = timepull1->GetXaxis(); TAxis * yachse = timepull1->GetYaxis();
 	xachse->SetTitleFont (132);
 	yachse->SetTitleFont (132);
@@ -153,7 +162,7 @@ void plot_resolution_sim_2g(){
 
 	RooHist* hpull2 = frame2->pullHist();
 	RooPlot* timepull2 = time->frame(Title("Pull Distribution"), Bins(60), Range("long")) ;
-	//timepull2->addPlotable(hpull2,"P") ;
+	timepull2->addPlotable(hpull2,"P") ;
 	TAxis * xachse2 = timepull2->GetXaxis(); TAxis * yachse2 = timepull2->GetYaxis();
 	xachse2->SetTitleFont (132);
 	yachse2->SetTitleFont (132);
@@ -172,7 +181,7 @@ void plot_resolution_sim_2g(){
 	TPad* pad22 = new TPad("pad22","This is pad2",0.05,0.0,0.95,0.2);
 	pad12->Draw();
 	pad22->Draw();
-	pad12->cd() ; gPad->SetLeftMargin(0.15) ; frame2->GetYaxis()->SetTitleOffset(1) ; frame2->Draw() ;
+	pad12->cd() ; gPad->SetLogy(); gPad->SetLeftMargin(0.15) ; frame2->GetYaxis()->SetTitleOffset(1) ; frame2->Draw() ;
 	pad22->cd() ; gPad->SetLeftMargin(0.15) ; timepull2->GetYaxis()->SetTitleOffset(1) ; timepull2->Draw() ;
 	c2->SaveAs("plot_data_sim_2g_wide.pdf");
 }
