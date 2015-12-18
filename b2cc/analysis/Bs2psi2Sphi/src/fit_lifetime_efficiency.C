fcff699421044dd86b2ceaff945dbfb1162547b8
Greig Cowan <greig.cowan@gmail.com>
2015-12-18 18:01:57 +0000

    Small updates to efficiency fitting code

--- b2cc/analysis/Bs2psi2Sphi/src/fit_lifetime_efficiency.C
+++ b2cc/analysis/Bs2psi2Sphi/src/fit_lifetime_efficiency.C
@@ -66,10 +66,10 @@ vector<double> return_diffs(double f, double s1, double s2){
 }
 
 void fit(){
-    float tau_Bd(1.525);
-	//TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/tighter_cut/tight-mva.root");
-	//TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/no_mva/signal_proton.root"); tau_Bd = 1.520; // signal data
-	TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/no_mva/BdKpi-mc_proton_nomva.root"); tau_Bd = 1.525; // signal MC
+    float tau_Bd_val(1.525);
+	TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/tighter_cut/tight-mva.root"); tau_Bd_val = 1.520;
+	//TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/no_mva/signal_proton.root"); tau_Bd_val = 1.520; // signal data
+	//TFile *file = TFile::Open("/Disk/ecdf-nfs-ppe/lhcb/gcowan/Bs2psi2SKpi/B_s0/data/no_mva/BdKpi-mc_proton_nomva.root"); tau_Bd_val = 1.525; // signal MC
 	TTree *tree = (TTree*)file->Get("bCandidates");
 
 	RooRealVar *mass  = new RooRealVar("B_s0_Mass_JpsiConstr", "mass", 5200., 5400., "MeV/c^{2}");
@@ -85,10 +85,12 @@ void fit(){
 	RooRealVar *muminus_ProbNNmu = new RooRealVar("muminus_ProbNNmu", "muminus_ProbbNNpi", 0.,5300000., "MeV/c^{2}");
 	RooRealVar *ctime = new RooRealVar("B_s0_LOKI_DTF_CTAU", "ct",0.08991,4.1958, "ps");
 	RooRealVar *sigmat = new RooRealVar("B_s0_LOKI_DTF_CTAUERR", "sigmat",0.00,0.4);
+    RooRealVar *hlt1_trackmuon  = new RooRealVar("B_s0_Hlt1TrackMuonDecision_TOS", "hlt1_trackmuon", 0, 1);
+    RooRealVar *hlt1_trackalll0 = new RooRealVar("B_s0_Hlt1TrackAllL0Decision_TOS", "hlt1_trackalll0", 0, 1);
     RooRealVar *hlt1_unbiased = new RooRealVar("B_s0_Hlt1DiMuonHighMassDecision_TOS", "hlt1_unbiased", 0, 1);
     RooRealVar *hlt2_unbiased = new RooRealVar("B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS", "hlt2_unbiased", 0, 1);
 	RooRealVar *sweight = new RooRealVar("Nsig_sw", "sweight", -10000.0, 100000.0);
-	RooArgList *fitArgs = new RooArgList(*ctime, *hlt1_unbiased, *hlt2_unbiased, *sigmat, *sweight, *mass);
+	RooArgList *fitArgs = new RooArgList(*ctime, *hlt1_trackmuon, *hlt1_trackalll0, *hlt1_unbiased, *hlt2_unbiased, *sigmat, *sweight, *mass);
     fitArgs->add(*Kplus_PX);
     fitArgs->add(*Kplus_PY);
     fitArgs->add(*piminus_PX);
@@ -100,8 +102,9 @@ void fit(){
     fitArgs->add(*mKpi);
     fitArgs->add(*mPsi);
 	
-    //TCut cut1 = "(B_s0_Hlt1DiMuonHighMassDecision_TOS==1 && B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS==1)";
-    TCut cut1 = "abs(mkstar-896)<100&&sqrt(pow(Kplus_PX+piminus_PX,2)+pow(Kplus_PY+piminus_PY,2))>1000&&abs(psi_2S_M-3690)<40&&Kplus_ProbNNk>0.2&&piminus_ProbNNpi>0.2&&muminus_ProbNNmu>0.2&&muplus_ProbNNmu>0.2&&(B_s0_Hlt1DiMuonHighMassDecision_TOS==1) && (B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS==1)&&abs(B_s0_Mass_JpsiConstr-5279)<20";
+    TCut cut1 = "((B_s0_Hlt1DiMuonHighMassDecision_TOS==1||B_s0_Hlt1TrackMuonDecision_TOS==1||B_s0_Hlt1TrackAllL0Decision_TOS==1) && B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS==1) && abs(B_s0_Mass_JpsiConstr-5279)<25"; //signal
+    //TCut cut1 = "((B_s0_Hlt1DiMuonHighMassDecision_TOS==1||B_s0_Hlt1TrackMuonDecision_TOS==1||B_s0_Hlt1TrackAllL0Decision_TOS==1) && B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS==1) && abs(B_s0_Mass_JpsiConstr-5190)<20"; // background
+    //TCut cut1 = "abs(mkstar-896)<100&&sqrt(pow(Kplus_PX+piminus_PX,2)+pow(Kplus_PY+piminus_PY,2))>1000&&abs(psi_2S_M-3690)<40&&Kplus_ProbNNk>0.2&&piminus_ProbNNpi>0.2&&muminus_ProbNNmu>0.2&&muplus_ProbNNmu>0.2&&(B_s0_Hlt1DiMuonHighMassDecision_TOS==1) && (B_s0_Hlt2DiMuonDetachedPsi2SDecision_TOS==1)&&abs(B_s0_Mass_JpsiConstr-5279)<20";
 
 	RooDataSet * data1 = new RooDataSet("data1", "all input data", tree, *fitArgs, cut1);
     
@@ -117,16 +120,18 @@ void fit(){
 
 	RooRealVar *mean_g1 = new RooRealVar("g1 #mu","mean of g1",0.00);
 	RooRealVar *mean_g2 = new RooRealVar("g2 #mu","mean of g2",0.00,-0.1, 0.5);
-	RooRealVar *sf1 = new RooRealVar("sf1", "sf1", 0.043);
+	RooRealVar *sf1 = new RooRealVar("sf1", "sf1", 0.047);
 	RooRealVar *sf2 = new RooRealVar("sf2", "sf2", 5.0, 2.0, 10.);
 	RooRealVar *cf1 = new RooRealVar("cf1", "cf1", 0.010, -0.1, 0.1);
 	RooRealVar *cf2 = new RooRealVar("cf2", "cf2", 0.015, -0.1, 0.1);
 	RooRealVar *frac_g12 = new RooRealVar("frac_g12", "core fraction", 0.15, 0.01, 0.4);
 
-	RooRealVar *sigma_wide = new RooRealVar("#sigma wide","width of gwide",0.15,0.01,1.5);
-	RooRealVar *tau  = new RooRealVar("tau" , "tau" ,0.15, 0.1, 0.6);
-	RooRealVar *tau2 = new RooRealVar("tau2", "tau2", tau_Bd);
-	RooRealVar *DG = new RooRealVar("DG", "DG",0.0);
+	RooRealVar * sigma_wide = new RooRealVar("#sigma wide","width of gwide",0.15,0.01,1.5);
+	RooRealVar * tau  = new RooRealVar("tau" , "tau" ,0.15, 0.1, 0.6);
+	RooRealVar * tau_Bd = new RooRealVar("tau_Bd", "tau_Bd", tau_Bd_val, tau_Bd_val - 0.3, tau_Bd_val + 0.3);
+    //tau_Bd->setConstant();
+    RooGaussian *tau_Bd_constr = new RooGaussian("tau_Bd_constr","tau2_const", *tau_Bd, RooConst(tau_Bd_val), RooConst(0.004)) ;
+    RooRealVar *DG = new RooRealVar("DG", "DG",0.0);
 
 	RooGaussModel *gauss11 = new RooGaussModel("gauss11","gauss",*time, *mean_g1, *sf1);
 	RooGaussModel *gauss12 = new RooGaussModel("gauss12","gauss",*time, *mean_g1, RooConst(1), *sf2);
@@ -136,23 +141,24 @@ void fit(){
 	RooRealVar *frac_bdc1 = new RooRealVar("frac_bdc1", "core fraction2", 0.05,0.0,0.2);
 	RooRealVar *frac_tau2 = new RooRealVar("frac_tau2", "core fraction3", 0.05,0.0,0.5);
 
-	RooBDecay *bdc1 = new RooBDecay("bdc1","bdc1",*time,*tau,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *res1, RooBDecay::SingleSided);
-	RooBDecay *bdc12 = new RooBDecay("bdc12","bdc12",*time,*tau2,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *gauss11, RooBDecay::SingleSided);
-	RooAddPdf *total_bdc1 = new RooAddPdf("total_bdc1", "total_bdc1", RooArgList(*bdc12, *bdc1),RooArgList(*frac_tau2)); 
-	RooAddPdf *total1 = new RooAddPdf("total1", "total1", RooArgList(*gausswide, *total_bdc1, *res1),RooArgList(*frac_gwide1,*frac_bdc1)); 
+	RooBDecay *background = new RooBDecay("bdc1","bdc1",*time,*tau,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *res1, RooBDecay::SingleSided);
+	RooBDecay *signal     = new RooBDecay("bdc12","bdc12",*time,*tau_Bd,*DG,RooConst(1),RooConst(0), RooConst(0), RooConst(0),RooConst(0), *gauss11, RooBDecay::SingleSided);
+    RooAddPdf *total_decay = new RooAddPdf("total_bdc1", "total_bdc1", RooArgList(*signal, *background),RooArgList(*frac_tau2)); 
+	RooAddPdf *total       = new RooAddPdf("total1", "total1", RooArgList(*gausswide, *total_decay, *res1),RooArgList(*frac_gwide1,*frac_bdc1)); 
 
     
-	RooRealVar * beta    = new RooRealVar("beta", "beta", -0.05,-0.15,0.35);
-	RooRealVar * beta_sq = new RooRealVar("beta_sq", "beta_sq", 0.);
-	RooRealVar * p0   = new RooRealVar("p0", "p1", 0.);
-	RooRealVar * p1   = new RooRealVar("p1", "p1", 2.,0.,5.);
-	RooRealVar * p2   = new RooRealVar("p2", "p2", 2.,0.,5.);
-    RooFormulaVar * eff = new RooFormulaVar("eff","TMath::Power(@1*(@0-@5), @2)/(1 + TMath::Power(@1*(@0-@5),@2))*(1+@3*@0+@4*@0*@0)",RooArgList(*time, *p1, *p2, *beta, *beta_sq, *p0));
-    RooEffProd * modelEff = new RooEffProd("modelEff","model with efficiency", *bdc12, *eff);
+    // beta, beta_sq and p0 are never significant when they are floated in the fit. Nominal is to leave them fixed to zero.
+	RooRealVar * beta    = new RooRealVar("beta", "beta", 0.);//-0.05,-0.15,0.15);
+	RooRealVar * beta_sq = new RooRealVar("beta_sq", "beta_sq", 0.);// -0.1, 0.1);
+	RooRealVar * p0   = new RooRealVar("p0", "p1", 0.);//, -0.1, 0.1);
+	RooRealVar * p1   = new RooRealVar("p1", "p1", 1.,0.,3.);
+	RooRealVar * p2   = new RooRealVar("p2", "p2", 2.,0.,3.);
+    RooFormulaVar * eff = new RooFormulaVar("eff","(TMath::Power(@1*(@0-@5), @2)/(1 + TMath::Power(@1*(@0-@5),@2)))*(1+@3*@0+@4*@0*@0)",RooArgList(*time, *p1, *p2, *beta, *beta_sq, *p0));
+    RooEffProd * modelEff = new RooEffProd("modelEff","model with efficiency", *signal, *eff);
 
 	gROOT->ProcessLine(".x ~/lhcb/lhcbStyle.C");
     // Generate some events from the lifetime distribution
-    RooDataSet * theory_data = bdc12->generate(*time, 1e6);
+    RooDataSet * theory_data = signal->generate(*time, 1e6);
     const int nbins(40);
     double bins[nbins+1];
     const double t_min(0.3);
@@ -170,15 +176,17 @@ void fit(){
 
     TH1F * theory_hist = (TH1F*)theory_data ->createHistogram( "time", *time, RooFit::Binning(*binning) );
     TH1F *   data_hist = (TH1F*)       data1->createHistogram( "time", *time, RooFit::Binning(*binning) );
+    std::cout << data1->numEntries() << " " << theory_data->numEntries() << std::endl;
     theory_hist->Scale(data1->numEntries()/float(theory_data->numEntries()));
     data_hist->Divide(theory_hist);
     TFile * ratio_file = TFile::Open("Bd2psi2SKpi_time_efficiency.root","RECREATE");
     data_hist->Write();
     ratio_file->Close();
     RooDataHist * ratio = new RooDataHist("ratio", "ratio", RooArgList(*time), data_hist);
-	RooPlot * ratio_frame = time->frame(Binning(*binning));
+	//time->setBinning(*binning, "default");
+    RooPlot * ratio_frame = time->frame();
 
-	RooFitResult *fitresult = modelEff->fitTo(*data1,Save(kTRUE), NumCPU(6),SumW2Error(kFALSE)); 
+	RooFitResult *fitresult = modelEff->fitTo(*data1,Save(kTRUE), NumCPU(6),SumW2Error(kFALSE), ExternalConstraints(*tau_Bd_constr)); 
     
     RooPlot* frame1 = time->frame(Bins(60),Title("Prompt decay time1")) ;
 
